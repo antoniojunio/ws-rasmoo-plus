f0df3e65cf695af7c9d70e875d1404b1cbc77e6e
antoniojunio <juniordsj@gmail.com>
2024-06-25 20:02:46 -0300

    Initial commit

---
+++ src/main/java/com/client/ws/rasmooplus/WsRasmooPlusApplication.java
@@ -0,0 +1,14 @@
+package com.client.ws.rasmooplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsRasmooPlusApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WsRasmooPlusApplication.class, args);
+	}
+
+}
